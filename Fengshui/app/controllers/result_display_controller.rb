e3a0e6b74b630774e640b3098c97020cb48e7972
fengshuiapp <31821699+fengshuiapp@users.noreply.github.com>
2018-03-06 22:41:22 +0800

    final code v4

--- Fengshui/app/controllers/result_display_controller.rb
+++ Fengshui/app/controllers/result_display_controller.rb
@@ -204,34 +204,50 @@ class ResultDisplayController < UIViewController
                 @result_room = "Higher priority: Successful scholarly pursuits. Children will be intelligent and talented."
 
              elsif @facingdirection == "PING"or"WU"or"TING"
+
                    @result_room = "Higher priority: The head of the family will be injured in accidents. Children may be uncaring toward
                    their parents, and descendants may become thieves and vring harm to family members"
+
              else
+
                    @result_room = "Higher priority: No conditional and unconditional interactions."
+
              end
 
           elsif ((@plate_display[1][0]==2 and @plate_display[1][1]==7)or(@plate_display[1][0]==7 and @plate_display[1][1]==2))
+
                 if @facingdirection == "WEI"or"K'UN"or"SHEN"or"CH'OU"or"KEN"or"YIN"or"PING"or"WU"or"TING"
+
                    @result_room = "Higher priority: Finalcial success, but seedy connections may be involved."
+
                 elsif @facingdirection == "HUS"or"CH'IEN"or"HAI"or"KENG"or"YU"or"HSIN"
+
                       @result_room = "Higher priority: Infant death, illness and accidents."
                 else
                       @result_room ="Higher priority: No conditional and unconditional interactions."
                 end
 
           elsif ((@plate_display[1][0]==3 and @plate_display[1][1]==8)or(@plate_display[1][0]==8 and @plate_display[1][1]==3))
+
                 if @facingdirection == "PING"or"WU"or"TING"or"CHIA"or"MAO"or"I"or"CH'EN"or"SUN"or"SSU"
+
                    @result_room ="Higher priority: Success in politics or in producing caring and responsible Children."
+
                 elsif @facingdirection == "WEI"or"K'UN"or"SHEN"
+
                       @result_room = "Higher priority: Suicide. Children may die young."
                 else
                       @result_room="Higher priority: No conditional and unconditional interactions."
                 end
 
           elsif ((@plate_display[1][0]==4 and @plate_display[1][1]==9)or(@plate_display[1][0]==9 and @plate_display[1][1]==4))
+
                 if @facingdirection == "JEN"or"TZU"or"KUEI"or"KENG"or"YU"or"HSIN"or"HUS"or"CH'IEN"or"HAI"
+
                    @result_room="Higher priority: Tremendous success in business through ethical means."
+
                 elsif @facingdirection == "CHIA"or"MAO"or"I"
+
                       @result_room="Higher priority: Injury or death in war. Children will become orphans."
                 else
                       @result_room ="Higher priority: No conditional and unconditional interactions."
@@ -241,7 +257,6 @@ class ResultDisplayController < UIViewController
                 result_calculate(1)
           end
 
-
       end
 
       def result_calculate(i)
@@ -249,136 +264,214 @@ class ResultDisplayController < UIViewController
           if ((@plate_display[i][0]==1 and @plate_display[i][1]==4)or(@plate_display[i][0]==4 and @plate_display[i][1]==1))
 
              @result_room="Success in scholarly pursuits."
+
              if (i==4)
                 @result_room="Apply to all rooms:"+@result_room
              end
+
           elsif @plate_display[i][0]==6 and @plate_display[i][1]==8
+
                 @result_room ="Success in business or the military arts."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==8 and @plate_display[i][1]==6
+
                 @result_room = "Success in scholarly or artistic endeavors."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==4 and @plate_display[i][1]==6
+
                 @result_room="Brings talent and fame."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==8 and @plate_display[i][1]==9
+
                 @result_room="Many happy occasions in family life."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==9 and @plate_display[i][1]==8
+
                 @result_room="Enhances fame."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==2 and @plate_display[i][1]==8
+
                 if @facingdirection == "HSII"or"CH'EN"or"HAI"
+
                    @result_room="Great wealth."
+
                    if (i==4)
                       @result_room="Apply to all rooms:"+@result_room
                    end
+
                 else
                    @result_room="No conditional and unconditional interactions."
+
                    if (i==4)
                       @result_room="Apply to all rooms:"+@result_room
                    end
+
                 end
+
           elsif @plate_display[i][0]==3 and @plate_display[i][1]==1
+
                 if @facingdirection == "KENG"or"YU"or"BSIN"
+
                    @result_room="Many descendants."
+
                    if (i==4)
                       @result_room="Apply to all rooms:"+@result_room
                    end
+
                 else
+
                    @result_room="No conditional and unconditional interactions."
+
                    if (i==4)
                       @result_room="Apply to all rooms:"+@result_room
                    end
+
                 end
+
           elsif @plate_display[i][0]==7 and @plate_display[i][1]==9
+
                 @result_room="Problems associated with politics."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif ((@plate_display[i][0]==2 and @plate_display[i][1]==5)or(@plate_display[i][0]==5 and @plate_display[i][1]==2))
+
                 @result_room="Severe illness."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==9 and @plate_display[i][1]==7
+
                 @result_room="fire caused by human activity."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==2 and @plate_display[i][1]==7
+
                 @result_room="File caused by natural circumstances."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==5 and @plate_display[i][1]==5
+
                 @result_room="Severe illness or death."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif ((@plate_display[i][0]==5 and @plate_display[i][1]==9)or(@plate_display[i][0]==9 and @plate_display[i][1]==5))
+
                 @result_room="Signify accidents that lead to injury or death."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif ((@plate_display[i][0]==7 and @plate_display[i][1]==6)or(@plate_display[i][0]==6 and @plate_display[i][1]==7))
+
                 @result_room="Armed robbery and loss of fortunes.There is also the prossiblity of injuries."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif ((@plate_display[i][0]==3 and @plate_display[i][1]==2)or(@plate_display[i][0]==2 and @plate_display[i][1]==3))
+
                 @result_room="Quarrels and disharmony in relationships. If this combination is located in the kitchen, it signifies
                 having too many mouths to feed, that is, financial hard times for the family."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==7 and @plate_display[i][1]==3
+
                 @result_room="Unexpected fains in business, but these gains will invite robbery and trickery."
+
                 if (i==4)
                 @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==3 and @plate_display[i][1]==7
+
                 @result_room="Illness associated with worries and anxieties. There is also the prossiblity of being victimized by 
                 politics or being embroiled in legal disputes."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==6 and @plate_display[i][1]==9
+
                 @result_room="Illness involving the failure of internal organs. The eldest member of the family has the highest risk."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==7 and @plate_display[i][1]==9 and @plate_display[i][2]==5
+
                 @result_room="Terminal illness."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==8 and @plate_display[i][1]==4
+
                 @result_room="Children will be unhealthy."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==2 and @plate_display[i][1]==9
+
                 @result_room="Obstacels in business ventures."
+
                 if (i==4)
                    @result_room="Apply to all rooms:"+@result_room
                 end
+
           elsif @plate_display[i][0]==3 and @plate_display[i][1]==8
+
                 @result_room="Illness in Children."
+
                 if (i==4)
                 @result_room="Apply to all rooms:"+@result_room
                 end
+
           else 
+            
                 case  @plate_display[i][0]
 
                       when 1,4,6,8
@@ -429,12 +522,17 @@ class ResultDisplayController < UIViewController
                 if  @temp<0
 
                     @result_room = "Bad Fengshui."
+
                 elsif  
+
                     @temp == 0
                     @result_room = "Neutral."
+
                 elsif
+
                     @temp >0 
                     @result_room = "Good Fengshui."
+
                 end
 
                         
